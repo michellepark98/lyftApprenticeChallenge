395b21081f87c005e72a89c217a3b0628975088c
myoungjun.kang <myoungjun.kang@ticketmaster.com>
2022-05-13 13:12:45 -0700

    final changes to main file, allowing host to be any host

--- main.py
+++ main.py
@@ -1,30 +1,22 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
-from requests import request
-
 
 app = Flask(__name__)
 api = Api(app)
 
+string_put_args = reqparse.RequestParser()
+string_put_args.add_argument("string_to_cut", type=str, help="String is required", required=True)
 
-video_put_args = reqparse.RequestParser()
-video_put_args.add_argument("string_to_cut", type=str, help="String is required", required=True)
 
-
-#creating a resource
+#creating a Lyft resource, overriding the post method
 class Lyft(Resource):
 	def post(self):
-		args =  video_put_args.parse_args()
-
-
-
+		args =  string_put_args.parse_args()
 		return_json = {"return_string": args['string_to_cut'][2::3]}
-
 		return return_json, 201
-
 
 api.add_resource(Lyft, "/test")
 
 if __name__ == "__main__":
-	app.run(port=5000, debug=True)
+	app.run(host='0.0.0.0', port=5000, debug=True)
 
